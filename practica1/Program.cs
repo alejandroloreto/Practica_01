39512e115052ae5834113a7312f43656f5d1c83a
alejandroloreto <cano_als_10@hotmail.com>
2014-02-14 13:57:27 -0600

    practica 1

---
+++ practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alejandro Loreto Salas
+ * Date: 14/02/2014
+ * Time: 11:20 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
